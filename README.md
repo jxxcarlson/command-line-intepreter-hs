c0679e481df35bdd135e6c098848f9f4f5e9a4dc
James Carlson <jxxcarlson@gmail.com>
2021-01-10 10:50:34 -0500

    Add command /enter and wire it to Info.get, which gets a string and returns Maybe Info by attempting to parse the string into JSON.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 Skeleton command-line interpreter in Haskell
+
+### Sample session
+
+```
+$ stack run
+
+info > firstName: Jim
+lastName: XXX
+email: foo@bar
+/enter
+Just (Info {firstName = "Jim", lastName = "XXX", email = "foo@bar"})
+```
--- src/Exec.hs
+++ src/Exec.hs
@@ -1,6 +1,7 @@
 module Exec(exec) where 
 
 import Data.List.Utils (replace)
+import Info
  
 -- DISPATCHER
 
@@ -13,7 +14,10 @@ exec str =
      (cmd:args) -> 
        case cmd of
          "/help" -> putStrLn $ prefix ++ "No help file yet"
-         "/echo" -> putStrLn $ prefix ++ (drop 7 str)
+         "/enter" -> putStrLn $ show $ Info.get $ dropCommand "/enter" str
+         "/echo" -> putStrLn $ prefix ++ dropCommand "/echo" str -- drop 7 str)
          _ -> putStrLn  $ prefix ++  "I don't understand\n" ++ str
 
 
+dropCommand :: String -> String -> String 
+dropCommand cmd str = drop ((length cmd)+ 2) str
