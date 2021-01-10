2c05f0b3455ffbf90c41beec0d4cb70ce9080411
James Carlson <jxxcarlson@gmail.com>
2021-01-10 15:11:56 -0500

    - Add command /stat to count all records, all valid recors - Add command /display to display all records

--- src/Exec.hs
+++ src/Exec.hs
@@ -1,7 +1,9 @@
 module Exec(exec) where 
 
 import Data.List.Utils (replace)
+import Data.Maybe(catMaybes)
 import Info
+import qualified Data.Text.IO as TIO
  
 -- DISPATCHER
 
@@ -16,8 +18,25 @@ exec str =
          "/help" -> putStrLn $ prefix ++ "No help file yet"
          "/enter" -> putStrLn $ show $ Info.get $ dropCommand "/enter" str
          "/echo" -> putStrLn $ prefix ++ dropCommand "/echo" str -- drop 7 str)
+         "/stat" -> stat
+         "/display" -> Exec.display
          _ -> putStrLn  $ prefix ++  "I don't understand\n" ++ str
 
 
 dropCommand :: String -> String -> String 
 dropCommand cmd str = drop ((length cmd)+ 2) str
+
+
+display :: IO ()
+display = do
+    text <- TIO.readFile "data.txt"
+    let records = Info.getMany Info.sep text
+    putStrLn $ Info.displayMany $ catMaybes records
+
+
+stat :: IO ()
+stat = do
+    text <- TIO.readFile "data.txt"
+    let records = Info.getMany Info.sep text
+    putStrLn $ show $ (length records, length $ catMaybes records)
+
--- src/Info.hs
+++ src/Info.hs
@@ -1,7 +1,9 @@
-module Info(get) where  
+module Info(displayMany, get, getMany, sep) where  
 
 import Parser (Parser, field, char, runParser)
-
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Data.Maybe (catMaybes)
 
 data Info = Info {firstName :: String, lastName :: String, email:: String}
     deriving Show
@@ -9,14 +11,38 @@ data Info = Info {firstName :: String, lastName :: String, email:: String}
 
 info :: Parser Info
 info = do
-        fName <- Parser.field "firstName"  <* char '\n'
-        lName <- Parser.field "lastName"  <* char '\n'
-        email <- Parser.field "email" 
+        fName <- Parser.field "FirstName"  <* char '\n'
+        lName <- Parser.field "LastName"  <* char '\n'
+        email <- Parser.field "EmailAddress" 
         return (Info fName lName email)
 
+
+
+displayMany :: [Info] -> String
+displayMany infos = 
+    displayStrings $ map display infos
+ 
+display  :: Info -> String
+display info = 
+     (firstName info) ++ " " ++ (lastName info) ++ ": " ++ (email info)
+
+displayStrings :: [String] -> String
+displayStrings items = 
+    foldl (\item acc -> item ++ "\n" ++ acc) (head items) (drop 1 items)
+ 
 
 get :: String -> Maybe Info
 get str = 
     case Parser.runParser info str of
         (_, Left _) -> Nothing 
         (_, Right info_) -> Just info_
+
+
+getMany :: T.Text -> T.Text -> [Maybe Info]
+getMany sep text = map (\d -> get $ trimLeading $ T.unpack d) (T.splitOn sep text)
+
+sep = T.pack "----"
+
+
+trimLeading :: String -> String
+trimLeading = dropWhile (\c -> c == ' ' || c == '\n')
