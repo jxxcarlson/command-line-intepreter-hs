6d959f8ea4232e53049c51f3644c4139c1245c6d
James Carlson <jxxcarlson@gmail.com>
2021-01-14 08:49:08 -0500

    - Install SendGrid - Add comands: /mail, /setFile

--- src/Exec.hs
+++ src/Exec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Exec where 
 
 import Data.List.Utils (replace)
@@ -5,13 +7,19 @@ import Data.Maybe(catMaybes)
 import Info 
 import qualified Data.Text.IO as TIO
 import qualified Data.Text as T
+
+
  
 -- DISPATCHER
 
-data PState = PState { count :: Int, records :: [Maybe Info], message :: String}
+data PState = PState { count :: Int
+                     , records :: [Maybe Info]
+                     , message :: String
+                     , addressesPath :: String
+                     }
 
 initialPState :: PState
-initialPState = PState { count = 0, records = [], message = ""}
+initialPState = PState { count = 0, records = [], message = "", addressesPath = "test.txt"}
 
 prefix = "-----\n"
 
@@ -21,12 +29,40 @@ exec pState str =
      [] -> return pState { message = ""}
      (cmd:args) -> 
        case cmd of
-         "/help" -> return pState {message =  prefix ++ "No help file yet" }
+         "/help" -> help pState
+         "/setfile" -> setFile pState args
          "/enter" -> enter pState str
          "/echo" -> return pState {message = prefix ++ dropCommand "/echo" str}
          "/stat" -> stat pState
          "/display" -> display pState
+         "/mail" -> mail pState args
          _ -> return pState {message = prefix ++  "I don't understand\n" ++ str }
+
+
+
+help :: PState -> IO PState
+help pState = do
+  text <- TIO.readFile  "help.txt"
+  return pState { message = T.unpack text}
+
+
+
+mail :: PState -> [String] -> IO PState
+mail pState args =
+    case args of 
+        [] -> return pState {message = "No message file given"}
+        (filePath:_) ->
+            do
+            message <- TIO.readFile filePath
+            return pState { message = T.unpack message}
+
+setFile :: PState -> [String] -> IO PState
+setFile pState args =
+    case args of 
+        [] -> return pState {message = "No file name given"}
+        (filePath:_) ->
+            do
+            return pState { message = "addresses: " ++ filePath, addressesPath = filePath}
 
 
 
@@ -39,10 +75,10 @@ enter pState str =
         Nothing -> return pState {message = "Error parsing record"}
         Just rec -> 
             do
-                text <- TIO.readFile "data.txt"
+                text <- TIO.readFile (addressesPath pState)
                 let text' = T.append (Info.toText rec) text
                 let records = Info.getMany Info.sep text
-                TIO.writeFile "data.txt" text'
+                TIO.writeFile (addressesPath pState) text'
                 return pState {message = show rec, records = (Just rec):records}  
 
 
@@ -55,14 +91,14 @@ dropCommand cmd str = drop ((length cmd)+ 2) str
 
 display :: PState -> IO PState
 display pState = do
-    text <- TIO.readFile "data.txt"
+    text <- TIO.readFile (addressesPath pState)
     let records = Info.getMany Info.sep text
     return pState { records = records, message = Info.displayMany $ catMaybes records }
 
 
 stat :: PState -> IO PState
 stat pState = do
-    text <- TIO.readFile "data.txt"
+    text <- TIO.readFile (addressesPath pState)
     let records = Info.getMany Info.sep text
     return pState { records = records, message = show (length records, length $ catMaybes records) }
 
